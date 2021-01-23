b815df57aa79e79c6ae45190b6ddf35d8edd9501
Jin <thinkjin6@gmail.com>
2021-01-23 21:51:57 +0900

    Add local storage

--- script.js
+++ script.js
@@ -1,24 +1,84 @@
 'use strict';
 const formEl = document.querySelector('.form');
-const btnAdd = document.querySelector('.btn--add');
 const todo = document.querySelector('.todo');
 
-// Event listener
-formEl.addEventListener('submit', function (e) {
+// Get lists from local storage
+const localStorageLists = JSON.parse(localStorage.getItem('lists'));
+
+// lists which will be generated on DOM
+let lists = localStorage.getItem('lists') !== null ? localStorageLists : [];
+
+// Set local storage
+const setLocalStorage = function () {
+  localStorage.setItem('lists', JSON.stringify(lists));
+};
+
+// Update DOM
+const updateDOM = function () {
+  todo.innerHTML = '';
+  lists.forEach((list) => todo.insertAdjacentHTML('afterbegin', list.li));
+};
+
+// Render list on DOM
+const renderLists = function (e) {
   e.preventDefault();
-  const input = this.querySelector('.input');
-  const list = `
-        <div class="list--container">
-          <span class="list">${input.value}</span>
-          <div class="btn--container">
-            <button class="fas fa-check btn btn--check"></button>
-            <button class="btn btn--remove">X</button>
-          </div>
+
+  const randomID = Math.trunc(Math.random() * 1000000);
+  const input = document.querySelector('.input');
+
+  let list = {
+    id: randomID,
+    text: input.value,
+  };
+  list.li = `
+    <div class="list--container" data-id=${list.id}>
+        <span class="list">${list.text}</span>
+        <div class="btn--container">
+          <button class="fas fa-check btn btn--check"></button>
+          <button class="btn btn--remove">X</button>
         </div>
-  `;
+    </div>
+    `;
+
+  lists.push(list);
 
   if (input.value === '') return;
 
-  todo.insertAdjacentHTML('beforeend', list);
+  setLocalStorage();
+  updateDOM();
   input.value = '';
-});
+};
+
+// Delete list on DOM
+const deleteList = function (e) {
+  const data = +e.target.closest('.list--container').getAttribute('data-id');
+
+  if (!e.target.classList.contains('btn--remove')) return;
+
+  localStorage.clear();
+  lists = lists.filter((list) => list.id !== data);
+
+  setLocalStorage();
+
+  updateDOM();
+};
+
+// Move list to Checked list
+const moveToCheckedList = function (e) {
+  const data = +e.target.closest('.list--container').getAttribute('data-id');
+
+  if (!e.target.classList.contains('btn--check')) return;
+
+  const li = lists.filter((list) => list.id === data);
+  console.log(li);
+};
+
+// Init
+const init = (function () {
+  updateDOM();
+})();
+
+// Event listener
+formEl.addEventListener('submit', renderLists);
+todo.addEventListener('click', deleteList);
+todo.addEventListener('click', moveToCheckedList);
